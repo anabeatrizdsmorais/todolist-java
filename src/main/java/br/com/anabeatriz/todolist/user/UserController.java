35c358062a1cf664bc7398f7914d21a78f9c3eb3
Ana B Morais <anabeatrizdsmorais@gmail.com>
2023-10-10 14:34:45 -0300

    feat: interação do sistema com banco de dados h2

--- src/main/java/br/com/anabeatriz/todolist/user/UserController.java
+++ src/main/java/br/com/anabeatriz/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package br.com.anabeatriz.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,19 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     
+    @Autowired
+    private IUserRepository userRepository;
 
-    /**
-     * String (text)
-     * Integer (int)- numeros inteiros (0, 10)
-     * Double (double) - numeros 0.0000 
-     * Float (float) - numeros 0.000
-     * Char (A Z)
-     * Date (data)
-     * void
-     */
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
 
-    @PostMapping("")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null) {
+            System.out.println("Usuário ja existe!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário ja existe!");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     } 
 }
