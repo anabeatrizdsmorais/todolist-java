9a047552b99bc5815680c855fe909324e8cd6223
Ana B Morais <anabeatrizdsmorais@gmail.com>
2023-10-11 10:32:21 -0300

    feat: adicao de rota task e encriptação na senha

--- src/main/java/br/com/anabeatriz/todolist/user/UserController.java
+++ src/main/java/br/com/anabeatriz/todolist/user/UserController.java
@@ -2,12 +2,13 @@ package br.com.anabeatriz.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 /**
  * Modificadores
@@ -32,6 +33,11 @@ public class UserController {
             System.out.println("Usuário ja existe!");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário ja existe!");
         }
+
+        var passwordHashred = BCrypt.withDefaults()
+        .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
